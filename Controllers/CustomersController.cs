ebebc54b22be2b7abd8e0a10ce8dbbed61e8544b
Joaquin Ramirez <39752343+Tavera15@users.noreply.github.com>
2020-05-05 01:16:59 -0500

    Created customer model with labels

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InvoiceGen_ASPNET.Models;
+
+namespace InvoiceGen_ASPNET.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Get Customers from Database and display them
+        public ActionResult Index()
+        {
+            CustomerModel c1 = new CustomerModel("lui", "123 plaeground stret", "LS", "CA", "12345");
+            CustomerModel c2 = new CustomerModel("canoes", "345 rynx", "LSse", "CfeeA", "67890");
+
+            var customers = new List<CustomerModel>();
+            customers.Add(c1);
+            customers.Add(c2);
+
+            return View(customers);
+        }
+
+        // Display a form used to create a new customer and then send to DB
+        public ActionResult Create()
+        {
+            CustomerModel customer = new CustomerModel();
+            return View(customer);
+        }
+    }
+}
--- Models/CustomerModel.cs
+++ Models/CustomerModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,37 @@ namespace InvoiceGen_ASPNET.Models
 {
     public class CustomerModel
     {
+        [DisplayName("Customer Name")]
+        public string mName { get; set; }
+
+        [DisplayName("Customer Address")]
+        public string mAddress { get; set; }
+
+        [DisplayName("City")]
+        public string mCity { get; set; }
+
+        [DisplayName("State")]
+        public string mState { get; set; }
+
+        [DisplayName("Zip Code")]
+        public string mZipCode { get; set; }
+
+        public CustomerModel()
+        {
+            mName = "";
+            mAddress = "";
+            mCity = "";
+            mState = "";
+            mZipCode = "";
+        }
+
+        public CustomerModel(string name, string address, string city, string state, string zip)
+        {
+            mName = name;
+            mAddress = address;
+            mCity = city;
+            mState = state;
+            mZipCode = zip;
+        }
     }
 }
